e9915d7369cfd9db3f676c35faea1a6432e8cbab
Aaron France <aaron.l.france@gmail.com>
2020-02-29 17:39:10 +0100

    feat: modernize

--- golang.mk
+++ golang.mk
@@ -19,53 +19,13 @@ BUILD_FLAGS=-ldflags "\
 	-X '$(PACKAGE)/cmd.BuildHash=$(BUILD_HASH)' \
 	-X '$(PACKAGE)/cmd.BuildEnvironment=$(BUILD_USER)@$(BUILD_MACHINE)' \
 "
+test: vet lint test_packages
 
-GOFILES=$(shell find . -type f -name '*.go' -not -path "./vendor/*")
-GOPKGS=$(shell go list ./...)
-
-default: build
-
-Gopkg.lock: Gopkg.toml
-	dep ensure
-	touch Gopkg.lock
-
-vendor: Gopkg.lock Gopkg.toml
-	dep ensure
-	touch vendor
-
-format:
-	gofmt -s -w $(GOFILES)
-
-vet:
-	go vet $(GOPKGS)
-
-lint:
-	$(foreach pkg,$(GOPKGS),golint $(pkg);)
-
-test_gopath:
-	test $$(go list) = "$(PACKAGE)"
-
-test_packages: vendor
-	go test $(GOPKGS)
-
-test_format:
-	gofmt -l $(GOFILES)
-
-test: test_gopath test_format vet lint test_packages
-
-cov:
-	gocov test -v $(GOPKGS) \
-		| gocov-html > coverage.html
-
-build: vendor
+build:
 	go build \
 		$(BUILD_FLAGS) \
 		-o $(NAME)-$(BUILD_VERSION)-$(shell go env GOOS)-$(shell go env GOARCH)
 	ln -sf $(NAME)-$(BUILD_VERSION)-$(shell go env GOOS)-$(shell go env GOARCH) $(NAME)
-
-xc:
-	GOOS=linux GOARCH=amd64 make build
-	GOOS=darwin GOARCH=amd64 make build
 
 install: test
 	go install \
